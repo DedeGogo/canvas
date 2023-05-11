0f7d58c32b4cdae48c3a90a531417bdf087b2afd
Andres Gomez <andres@lilo.org>
2023-05-11 19:49:24 +0200

    Satrrt again, notes and working canvas

--- src/canvas/views.cljs
+++ src/canvas/views.cljs
@@ -1,59 +1,90 @@
 (ns canvas.views
   (:require
+   [reagent.core :as reagent]
    [re-frame.core :as rf]
+   [reagent.dom :as rdom]
    [canvas.data   :as d]))
 
-(def pi (.-PI js/Math ))
 
-(def test-element (first d/data))
+#_(defn canvas-container
+    []
+    (let [style {:width  1000
+                 :height 1000
+                 :background-color "#333333"}]
+      [:canvas {:id "main" :style style}]))
 
-;; Use (-> ctx 
-;;         (.method args))
-;; Canvas methods :
-;; .fillRect takes 4 args top left width height => rectangle
+(defn canvas []
+  (reagent/create-class
+   {:component-did-mount
+    (fn [comp]
+      (let [node (rdom/dom-node comp)
+            ctx (.getContext node "2d")]
+        (.fillRect ctx 10 10 10 10)
+        (.fillRect ctx 691 48 104 4)))
+    :reagent-render
+    (fn []
+      [:canvas {:width 1000 :height 1500 :style {:background-color "grey"}}])}))
+
+(defn main-panel []
+  [:div
+   [canvas]])
+(comment
+  #_(def canvas (.getElementById js/document "main"))
+  #_(set! (.-width canvas) "2000px")
+  #_(set! (.-height canvas) "2000px")
+
+  #_(def ctx (.getContext canvas "2d"))
+
+  (.fillRect ctx 60 30 61 61)
+  (def two-pi (* 2 (.-PI js/Math)))
+  (def test-element (first d/data)))
+
+;; Notes
+;; Part 1 display the shapes
+;; Todo, fix canvas display => use proper reagent with hooks, component did mount
+;; type 3 component
+
+;; Todo: how to display negative y or x values? 
+;;      use ctx.translate, where too? => find out min x and y vals in data
+
+;; Todo: Draw elements
+;;      draw rectangle => .fillRect takes 4 args top left width height => rectangle
 ;; -- problems some rect are filled and some are not, but I cant 
-;; -- see how to tell in data
-;; .arc takes 4 args center-top center-left radius 
-;; start-angle end-angle in radians (0 and 2π for a full circle)
-;; ---how to tell if it is full or just the border?
-;; ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-;; .fillStyle takes one arg a color
-;; this gives me all but the group. I will put it either in the 
-;; :key (str (:group) "-"(get-index)) => :key 0-23, or
-;; put all elements in app-db and subscribe to the group
+;;    see how to tell in data
+;;       Draw Cicle:
+;;              function drawCircle() {
+;;               ctx.beginPath()
+;;               ctx.arc(circle.x, circle.y, circle.size, 0, pi)
+;;               ctx.fillStyle = 'purple'
+;;               ctx.fill() or ctx.stroke if empty
+;;              }
 
-;; If I run (map draw-element d/data) it should draw all the elements,
-;; the repl returns nil, as it should.
-;; but nothing is rendered. 
-;; I think the problem is that for some reason I am supper zoomed in 
-;; on the canvas, but I dont know why and I can't find any info.
-;; the border of the images are very blury, which supports the idea 
-;; of being zoomed in, as does the fact that the pixel values are not rendred
-;; in "view port" pixels but un some thing larger.
+;;       Make proper cljs fns and test them
 
-;; I have spent most of my time with this bug. But I just saw the time, and
-;; it has been over two hours.
-
-;; I initially had the canvas in the main-panel in this ns, but since I needed it
-;; to load before I define canvas and ctx so I set it up temporarily
-;; on index.html the bug was there before the move.
-;; 
-
-;; the more I increase the width and height of the canvas, 
-;; the more zommed out I am. It is like it keeps the original 
-;; 300px by 150px size contained in the defined height and width
-
-;; if I set! the values after the fact, it seems to break
-
-;; but I will stop now.
+;;       Make map fn to draw elements programatically
 
 
-(def canvas (.getElementById js/document "main"))
-#_(set! (.-width canvas) "2000px")
-#_(set! (.-height canvas) "2000px")
+;; Part 2 figure how to draw an outline around the perimetter or a group of shapes
+;;    event handlers only recover mouse possition, 
+;;    there is no automatic way to identify each separate element or group
+;;    also elements cannor hold data, so the groups need to be defined 
+;;    somewhere else or someother way
+;;   
 
-(def ctx (.getContext canvas "2d"))
-(.beginPath ctx)
+;; Todo: figure out the perimeter of each group
+;;     sort elements by group
+;;     determine the the top left and bottom right corner of each group
+
+;; Todo: Envent listener
+;;     addEventListener("click", (event) => {});
+;;    where does it go? is it in component did mount?
+
+;; Todo: Write logic to dermine if the mouse is clicked over a group (cond ) 
+         
+
+;; Todo: update function that redraws the canvas with a strokeRect of the proper size over the right area
+;; or just the components of clicked outside the bounds of a group
+
 
 (comment
   #_{:shape-type :rect,
@@ -66,52 +97,49 @@
   (set! (.-fillStyle ctx) "rgb(0,0,255)")
   (.beginPath ctx)
   (.rect ctx 24 30 61 61)
-  (.fillRect ctx 60 30 61 61)
+  (.fillRect ctx 691 -48 104 4)
   (.fill ctx)
   (.reset ctx)
-  )
 
-(defn define-color [fill] (set! (.-fillStyle ctx) fill))
 
-(comment 
-  
+  (defn define-color [fill] (set! (.-fillStyle ctx) fill)))
+
+(comment
+
   (define-color "red")
-  
-  (.beginPath ctx)
-  
-  (.rect ctx 20 30 104 61)
-  )
 
-(defn draw-element [{:keys [shape-type x y width height radius fill]}]
-  (js/console.log (str {:shape shape-type :x x :y y :widht width :h height :r radius :fill fill}))
-  (define-color fill) 
-  (if
-    (= shape-type :rect)
-    (.rect ctx x y width height)
-    
-    (.arc ctx x y radius 0 pi))
-   (.fill ctx)
-  )
+  (.beginPath ctx)
+
+  (.rect ctx 20 30 104 61)
+
+
+  (defn draw-element [{:keys [shape-type x y width height radius fill]}]
+    (js/console.log (str {:shape shape-type :x x :y y :widht width :h height :r radius :fill fill}))
+    (define-color fill)
+    (if
+     (= shape-type :rect)
+      (.rect ctx x y width height)
+
+      (.arc ctx x y radius 0 two-pi))
+    (.fill ctx)))
 (comment
   (def e-test {:shape-type :rect,
-             :x 20,
-             :y 30,
-             :width 10,
-             :height 61,
-             :fill "#1AAE9F", ;; teal
-             :group 0})
+               :x 20,
+               :y 30,
+               :width 10,
+               :height 61,
+               :fill "#1AAE9F", ;; teal
+               :group 0})
 
   (map draw-element d/data)
-  
+
   (draw-element e-test)
-  
+
 
 
   (set! (.-fillStyle ctx) "grey")
-  
-  (-> ctx
-      (.fillRect 7 6 104 61))
-  )
 
-(defn main-panel []
-[:div ])
+  (-> ctx
+      (.fillRect 7 6 104 61)))
+
+
